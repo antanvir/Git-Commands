ef0529c8817dc5565589eba6499dffb8d028c649
A. N. TANVIR <bsse0904@iit.du.ac.bd>
2021-03-14 20:35:50 +0600

    Delete Push to an existing project.sh

--- Push to an existing project.sh
+++
@@ -1,9 +0,0 @@
-Go To THe Target Directory First Where Project Is Located.
-
-git init
-git remote add origin https://github.com/antanvir/UserProfileTemplate-Android (git remote add origin <repository URL>)
-git add .
-git commit -m "Firebase Authentication, storage & Database added" ( git commit -m "<some text>" )
-git push -f origin master/ git push -u origin master/git push origin master
-
-
