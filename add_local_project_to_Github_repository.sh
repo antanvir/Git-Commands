336e3492710488991f1212fb45a0997b9ee0493f
A. N. TANVIR <bsse0904@iit.du.ac.bd>
2021-03-14 20:44:23 +0600

    Update add_local_project_to_Github_repository.sh

--- add_local_project_to_Github_repository.sh
+++ add_local_project_to_Github_repository.sh
@@ -7,4 +7,4 @@ git remote add origin https://github.com/antanvir/UserProfileTemplate-Android   
 # Upload all local files and folders from the directory
 git add .
 git commit -m "setting up project"                                                [ git commit -m "some commit message" ]
-git push origin master / git push -f origin master                                [ -f for force update ]
+git push origin <branch_name> / git push -f origin <branch_name>                  [ -f for force update, for 'master' branch it would be: git push origin master ]
